d0c36301f3917638a1f0aaab0f6aa5952538523d
Altair <49795711+TMAltair@users.noreply.github.com>
2019-09-18 20:03:25 +0100

    Add files via upload

--- Rougalike.py
+++ Rougalike.py
@@ -4,8 +4,8 @@ import os        #Does file opperations and other stuff like clearing screen
 import random    #Allows random number generation
 
 #Select     - deals with user input Select.upper should allways follow after it                                         #slots.txt lines
-#DataPath   - Path to data folder                                                                                       #1 -Terrain
-#LoadStage  - Stage of loading                                                                                          #2 -Resource A
+#DataPath   - Path to data folder                                                                                       #1 - Terrain
+#LoadStage  - Stage of loading                                                                                          #2 - Hostiles
 #TempFile   - Current file that needs loading for linecache
 #SlotNo     - Picks a random number
 #Terrain    - Current Terrain
@@ -21,53 +21,22 @@ Turn = 0
 Character = DataPath + "\\char" 
 MapX = random.randint(-100000,100000)
 MapY = random.randint(-100000,100000)
+PLMXMana = 100
+PLMXHealth = 100
+PLMXAttack = 100
+EXP = 0
+PLMana = PLMXMana
+PLHealth = PLMXHealth
+PLAttack = PLMXAttack
 
 def MainMenu():
     Select = input("Project4\nNew game\n\n")
     Select = Select.upper() #Converts to caps so it cannot be missinterpreted due to case
     if Select == "NEW GAME":
-        CharCreate()
+        LoadInit()
     else:
         print("Invalid Command.")
         MainMenu()
-
-def CharCreate():
-    global Name
-    global MXMana
-    global MXHealth
-    global MXStamina
-    global MXSpeed
-    Name = input("Enter your name: ")
-    while LoadStage == 1:        
-        Stat = input("Pick your starting stat boost (Health/Mana/Stamina/Speed) ")
-        Stat = Stat.upper()
-        if Stat == "HEALTH":
-            MXHealth = 125
-            MXMana = 100
-            MXStamina = 100
-            MXSpeed = 100
-            LoadInit()
-        elif Stat == "MANA":
-            MXHealth = 100
-            MXMana = 125
-            MXStamina = 100
-            MXSpeed = 100
-            LoadInit()        
-        elif Stat == "STAMINA":
-            MXHealth = 100
-            MXMana = 100
-            MXStamina = 125
-            MXSpeed = 100
-            LoadInit()
-        elif Stat == "SPEED":
-            MXHealth = 100
-            MXMana = 100
-            MXStamina = 100
-            MXSpeed = 125
-            LoadInit()
-        else:
-            print("Invalid stat, Please pick Health/Mana/Stamina/Speed please")
-
 
 def LoadInit():         #Initalises loading 
     global LoadStage    #Makes a global variable editable
@@ -82,6 +51,8 @@ def Generation():   #Generates world
     elif LoadStage == 3:
         ResBGen()
     elif LoadStage == 4:
+        Creature()
+    elif LoadStage == 5:
         display()
 
 def TerrainGen():
@@ -122,6 +93,26 @@ def ResBGen():  #Generates 2nd resource (This is the exact same as ResB)
     LoadStage = LoadStage + 1
     Generation()
 
+def Creature():  #Sorts out Hostiles
+    global TempFile
+    global SlotNo
+    global LoadStage
+    global Terrain
+    global Enemy
+    global EMMana
+    global EMHealth
+    global EMAttack
+    TempFile = DataPath + "\\slots.txt"              #Writes the full path to slots.txt to TempFile
+    CreatureSlots = int( linecache.getline(TempFile, 2) )
+    SlotNo = random.randint(1,CreatureSlots)         #Picks a random number between 1 and how many slots are in line 1 of slots.txt
+    TempFile = DataPath + "\\hostile.txt"            #Path to hostile.txt
+    Enemy = linecache.getline(TempFile,SlotNo)     #Gets hostile
+    LoadStage = LoadStage + 1
+    EMMana = random.randint(1, PLMXMana*1.5)
+    EMHealth = random.randint(1, PLMXHealth*1.5)
+    EMAttack = random.randint(1, PLMXAttack*1.5)
+    Generation()
+
 def display():
     global Turn
     Turn = Turn + 1
@@ -130,23 +121,122 @@ def display():
     global ResBName
     print("There are",ResANum,ResAName)
     print("There are",ResBNum,ResBName)
-    PlayerTurn()    
+    PlayerTurn()
 
 def PlayerTurn():   #Player turn
     global Turn
     global Character
-
+    print("There is a",Enemy)
     print("Turn: ",Turn)
-    print("Actions:\nMove\nWait")
+    print("Actions:\nMove\nWait\nBattle")
     Select = input()
     Select = Select.upper()
     if Select == "WAIT":
         display()
     elif Select == "MOVE":
-        
-        LoadInit()
+        Move()
+    elif Select == "BATTLE":
+        Battle()
     else:
         print("Invalid command.")
         PlayerTurn()
+
+def Move(): #Remember Add map Perminance
+    #Add a check for a file with the maps name
+    global MapX
+    global MapY
+    Select = input("Move in what direction:\nNorth, South, East & West: ")
+    Select = Select.upper()
+    if Select == "NORTH":
+        MapY = MapY + 1
+    elif Select == "SOUTH":
+        MapY = MapY - 1
+    elif Select == "EAST":
+        MapX = MapX + 1
+    elif Select == "WEST":
+        MapX = MapX - 1
+    else:
+        print("Invalid Command")
+        Move()
+    LoadInit()
+
+def Battle():
+    global PLMana
+    global PLHealth
+    global PLAttack
+    global PLMXMana
+    global PLMXHealth
+    global PLMXAttack
+    global EMMana
+    global EMHealth
+    global EMAttack
+    global EXP
+    global Enemy
+    PLMana = PLMXMana
+    PLHealth = PLMXHealth
+    PLAttack = PLMXAttack
+    print("There is a",Enemy," Attack / Defend / Flee\n\n Your HP:",PLHealth,"\nEnemy HP:",EMHealth)
+    Select = input()
+    Select = Select.upper()
+    if Select == "ATTACK":
+        EMHealth = EMHealth - random.randint(PLAttack/2,PLAttack*2)
+        PLHealth = PLHealth - EMAttack
+    elif Select == "DEFEND":
+        PLHealth = PLHealth - EMHealth / 2
+    elif Select == "FLEE":
+        display()
+    else:
+        print("Invalid Command.")
+
+    if EMHealth <= 0 :
+        print("Congratulations! You defeated",Enemy)
+        MX = EMAttack + EMMana / 2
+        EXP = EXP + MX
+        display()
+    elif PLHealth <= 0:
+        print("You died. Press enter to restart")
+        input()
+        LoadInit()
+    else:
+        Battle2()
+
+def Battle2():
+    global PLMana
+    global PLHealth
+    global PLAttack
+    global PLMXMana
+    global PLMXHealth
+    global PLMXAttack
+    global EMMana
+    global EMHealth
+    global EMAttack
+    global EXP
+    global Enemy
+    print("There is a",Enemy," Attack / Defend / Flee\n\n Your HP:",PLHealth,"\nEnemy HP:",EMHealth)
+    Select = input()
+    Select = Select.upper()
+    if Select == "ATTACK":
+        EMHealth = EMHealth - random.randint(PLAttack/2,PLAttack*2)
+        PLHealth = PLHealth - EMAttack
+    elif Select == "DEFEND":
+        PLHealth = PLHealth - EMHealth / 2
+    elif Select == "FLEE":
+        display()
+    else:
+        print("Invalid Command.")
+
+    if EMHealth <= 0 :
+        print("Congratulations! You defeated",Enemy)
+        MX = EMAttack + EMMana / 2
+        EXP = EXP + MX
+        display()
+    elif PLHealth <= 0:
+        print("You died. Press enter to restart")
+        input()
+        LoadInit()
+    else:
+        Battle2()
+
+
 
 MainMenu()
