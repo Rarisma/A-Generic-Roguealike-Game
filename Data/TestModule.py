e4dd66b0f062aa8568f53bec1b26d982a35258a7
Altair <49795711+TMAltair@users.noreply.github.com>
2020-01-29 22:25:29 +0000

    Started refining code and modulizing

---
+++ Data/TestModule.py
@@ -0,0 +1,5 @@
+def __init__(self):
+    print("Initalized")
+
+def TMAltair():
+    print("Test passed!")
---
+++ Rougalike (New).py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__) + "\Data\\") # Allows modules in \data\ to be imported  
+
+import Data.TestModule
+TestModule.TMAltair()
